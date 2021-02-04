46d94c672c516a6e3ac4191931546bf9346c18ab
Benjamin Tagtow <bentagtow@gmail.com>
2021-02-04 11:54:55 -0700

    basic header and pic added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div>
+
+        <h1>Camper's</h1>
+
+        <h1 id="beans">Beans</h1>
+        <h1 id="burritos">Burritos</h1>
+        <h2>"mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm!"</h2>
+        {/* <img className="image" src="https://i.imgur.com/aSiElYD.jpg" title="source: imgur.com" alt="professional picture of me" /> */}
+        <img className="image" src="https://i.imgur.com/plgfTHh.jpeg" alt="camper"></img>
+      </div>
+      <div>
+        <blockquote class="imgur-embed-pub" lang="en" data-id="a/uQmx6FA" data-context="false" ><a href="//imgur.com/a/uQmx6FA"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
+      </div>
     </div>
   );
 }
